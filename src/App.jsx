e81e1406445f7b43871275b3df2125d09e0e251b
taorminak <ostanina.anna@yahoo.com>
2023-02-18 23:14:31 +0100

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css';
+import styles from './App.css';
+import words from "./components/all-words.json";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Card/>
       </header>
     </div>
   );

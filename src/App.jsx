4b616381884c43b71ac41973de4afcf5bff0e7e3
taorminak <ostanina.anna@yahoo.com>
2023-02-19 13:52:17 +0100

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,47 @@ import words from "./components/all-words.json";
 import Card from "./components/Card/Card";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+//import 'react-data-grid/lib/styles.css';
+//import DataGrid from 'react-data-grid';
+
+//const columns = [
+//  { key: 'id', name: 'ID' },
+//  { key: 'deutsch', name: 'Deutsch' },
+/*  { key: 'artikel', name: 'Artikel' },
+  { key: 'bedeutung', name: 'Bedeutung' },
+  { key: 'russisch', name: 'Russisch' },
+  { key: 'beispiele', name: 'Beispiele' }
+];
+ 
+
+const rows = words.map((word, index) => (
+  <Card key={index}
+  deutsch={word.deutsch}
+  artikel={word.artikel}
+  bedeutung={word.bedeutung}
+  russisch={word.russisch}
+  beispiele={word.beispiele}
+/>
+));
+<DataGrid columns={columns} rows={rows} />
+*/
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
-      {words.map((word, index) => (
-        <Card
-          key={index}
-          deutsch={word.deutsch}
-          artikel={word.artikel}
-          bedeutung={word.bedeutung}
-          russisch={word.russisch}
-          beispiele={word.beispiele}
-        />
-      ))}
+      <div className={styles.WordsContainer}>
+        {words.map((word, index) => (
+          <Card
+            key={index}
+            deutsch={word.deutsch}
+            artikel={word.artikel}
+            bedeutung={word.bedeutung}
+            russisch={word.russisch}
+            beispiele={word.beispiele}
+          />
+        ))}
+      </div>
       <Footer />
     </div>
   );

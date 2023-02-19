d0028c3f2f685d542447ffc0ba7d0b614cb9182b
taorminak <ostanina.anna@yahoo.com>
2023-02-19 20:12:57 +0100

    Grid updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import styles from "./App.css";
+import style from "./App.css";
 import words from "./components/all-words.json";
 import Card from "./components/Card/Card";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import style from "react-data-grid/lib/styles.css";
+import "react-data-grid/lib/styles.css";
 import DataGrid from "react-data-grid";
 
 const columns = [
@@ -15,22 +15,11 @@ const columns = [
   { key: "beispiele", name: "Beispiele" },
 ];
 
-
-
 function App() {
   return (
-    <div className={styles.App}>
+    <div className={style.App}>
       <Header />
-      <DataGrid className={style} columns={columns} rows={words.map((word, id) => (
-  <Card
-    key={id}
-    deutsch={word.deutsch}
-    artikel={word.artikel}
-    bedeutung={word.bedeutung}
-    russisch={word.russisch}
-    beispiele={word.beispiele}
-  />
-))}/>
+      <DataGrid columns={columns} rows={words} />
       <Footer />
     </div>
   );

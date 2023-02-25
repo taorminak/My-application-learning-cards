04ab95ade35d56befcd83cbdb2f3bd68937f7181
taorminak <ostanina.anna@yahoo.com>
2023-02-25 22:24:38 +0100

    Flipping Card added

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,30 @@ import words from "./components/all-words.json";
 import Card from "./components/Card/Card";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import "react-data-grid/lib/styles.css";
-import DataGrid from "react-data-grid";
+import { useState } from "react";
 
-const columns = [
-  { key: "id", name: "ID" },
-  { key: "deutsch", name: "Deutsch" },
-  { key: "artikel", name: "Artikel" },
-  { key: "bedeutung", name: "Bedeutung" },
-  { key: "russisch", name: "Russisch" },
-  { key: "beispiele", name: "Beispiele" },
-];
 
 function App() {
+const [count, setCount] = useState(0);
+
+function handlePrev() {
+  setCount(count - 1)
+}
+
+function handleNext() {
+  setCount(count + 1)
+}
+
+console.log(words[count].deutsch)
   return (
     <div className={style.App}>
       <Header />
-      <DataGrid columns={columns} rows={words} />
+      <div className={style.FlippingCard}>
+        <Card words={words[count].deutsch}/>
+        <div className={style.ButtonContainer}>
+        <button className={style.Button}  onClick={handlePrev}>Previous</button>
+        <button className={style.Button}  onClick={handleNext}>Next</button></div>
+      </div>
       <div className={style.Card}>
         {words.map((word) => (
           <Card
